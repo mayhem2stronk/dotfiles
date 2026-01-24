3a871b499d684e78575e1a452dd91b84b5a513d9
Lucas Hilbert <lucas@hi-1.de>
2026-01-24 21:03:30 +0100

    .

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,31 @@
+if status is-interactive
+    # Starship custom prompt
+    starship init fish | source
+
+    # Direnv + Zoxide
+    command -v direnv &>/dev/null && direnv hook fish | source
+    command -v zoxide &>/dev/null && zoxide init fish --cmd cd | source
+
+    # Better ls
+    alias ls='eza --icons --group-directories-first -1 --show-symlinks'
+
+    # Custom colours
+    #cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
+
+    # For jumping between prompts in foot terminal
+    function mark_prompt_start --on-event fish_prompt
+        echo -en "\e]133;A\e\\"
+    end
+
+    function y
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        command yazi $argv --cwd-file="$tmp"
+        if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+            builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+    end
+
+    #pokeget random --hide-name
+    fastfetch
+end
